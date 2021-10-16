9a29d064c778572bae6d3eac154831ed868ab30e
toms74209200 <motomotomato@gmail.com>
2021-10-16 23:00:55 +0900

    :sparkles: add LED blinck code

---
+++ src/chapt1/blink/blink.ino
@@ -0,0 +1,43 @@
+#define LED_R 18
+#define LED_G 19
+#define LED_B 20
+
+#define INTERVAL 200
+
+void setup() {
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
+
+  led_off(LED_R);
+  led_off(LED_G);
+  led_off(LED_B);
+}
+
+void loop() {
+  led_on(LED_R);
+  delay(INTERVAL);
+
+  led_off(LED_B);
+  delay(INTERVAL);
+
+  led_on(LED_G);
+  delay(INTERVAL);
+
+  led_off(LED_R);
+  delay(INTERVAL);
+
+  led_on(LED_B);
+  delay(INTERVAL);
+
+  led_off(LED_G);
+  delay(INTERVAL);
+}
+
+void led_on(int pin) {
+  digitalWrite(pin, LOW);
+}
+
+void led_off(int pin) {
+  digitalWrite(pin, HIGH);
+}
